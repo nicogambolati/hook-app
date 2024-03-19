fa7e9c413eb6e8c6a82fe11f63899af193d3a4a9
Nico Gambolati <nicolasgambolati@gmail.com>
2024-03-19 17:06:38 -0300

    useForm() - custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { HooksApp } from "./HooksApp";
-import { CounterApp } from "./useState/CounterApp";
-import { CounterWithCustomHook } from "./useState/CounterWithCustomHook";
-import { SimpleForm } from "./useEffect/SimpleForm";
+// import { CounterApp } from "./useState/CounterApp";
+// import { CounterWithCustomHook } from "./useState/CounterWithCustomHook";
+// import { SimpleForm } from "./useEffect/SimpleForm";
+import { FormWithCustomHook } from "./useEffect/FormWithCustomHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HooksApp />
     {/* <CounterApp />
     <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </React.StrictMode>
 );
---
+++ src/useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,49 @@
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+  const { formState, onInputChange, onResetForm, username, email, password } =
+    useForm({
+      username: "",
+      email: "",
+      password: "",
+    });
+
+  // const { username, email, password } = formState;
+
+  return (
+    <>
+      <h1>Formulario con Custom Hook</h1>
+      <hr />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="username"
+        name="username"
+        onChange={onInputChange}
+        value={username}
+      />
+
+      <input
+        type="text"
+        className="form-control mt-3"
+        placeholder="example@gmail.com"
+        name="email"
+        onChange={onInputChange}
+        value={email}
+      />
+
+      <input
+        type="password"
+        className="form-control mt-3"
+        placeholder="contraseña"
+        name="password"
+        onChange={onInputChange}
+        value={password}
+      />
+
+      <button className="btn btn-primary mt-3" onClick={onResetForm}>
+        Reset
+      </button>
+    </>
+  );
+};
