cac2996b42c5bb9462cbc5fc87b7d0ea2f310c28
Nico Gambolati <nicolasgambolati@gmail.com>
2024-03-19 21:51:32 -0300

    removes commented lines

--- src/main.jsx
+++ src/main.jsx
@@ -3,29 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { HooksApp } from "./HooksApp";
-// import { CounterApp } from "./useState/CounterApp";
-// import { CounterWithCustomHook } from "./useState/CounterWithCustomHook";
-// import { SimpleForm } from "./useEffect/SimpleForm";
-// import { FormWithCustomHook } from "./useEffect/FormWithCustomHook";
-// import { MultipleCustomHooks } from "./examples/MultipleCustomHooks";
-// import { FocusScreen } from "./useRef/FocusScreen";
-// import { Memorize } from "./memos/Memorize";
-// import { MemoHook } from "./memos/MemoHook";
-// import { CallbackHook } from "./memos/CallbackHook";
+import { CounterApp } from "./useState/CounterApp";
+import { CounterWithCustomHook } from "./useState/CounterWithCustomHook";
+import { SimpleForm } from "./useEffect/SimpleForm";
+import { FormWithCustomHook } from "./useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./examples/MultipleCustomHooks";
+import { FocusScreen } from "./useRef/FocusScreen";
+import { Memorize } from "./memos/Memorize";
+import { MemoHook } from "./memos/MemoHook";
+import { CallbackHook } from "./memos/CallbackHook";
 import { Padre } from "./tarea-memo/Padre";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HooksApp />
-    {/* <CounterApp />
-    <CounterWithCustomHook /> */}
-    {/* <SimpleForm /> */}
-    {/* <FormWithCustomHook /> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <FocusScreen /> */}
-    {/* <Memorize /> */}
-    {/* <MemoHook/>*/}
-    {/* <CallbackHook /> */}
-    <Padre/>
+    <CounterApp />
+    <CounterWithCustomHook />
+    <SimpleForm />
+    <FormWithCustomHook />
+    <MultipleCustomHooks />
+    <FocusScreen />
+    <Memorize />
+    <MemoHook />
+    <CallbackHook />
+    <Padre />
   </React.StrictMode>
 );
