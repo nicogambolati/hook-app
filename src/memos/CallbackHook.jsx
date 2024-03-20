d77459b773fa63ee87a7c0a239c0844cdc0a35be
Nico Gambolati <nicolasgambolati@gmail.com>
2024-03-19 21:37:35 -0300

    useCallback() - with arguments

--- src/memos/CallbackHook.jsx
+++ src/memos/CallbackHook.jsx
@@ -5,17 +5,17 @@ export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
   const incrementFather = useCallback(
-    () => {
+    (value) => {
         // Le mando un callback en el cual tengo el valor actual del counter y le sumo 1
-        setCounter((value) => value + 1);
+        setCounter((c) => c + value);
     },
     [],
   );
 
   // Solo se dispara cuando el la funcion
-  useEffect(() => {
-    incrementFather();
-  }, [incrementFather])
+//   useEffect(() => {
+//     incrementFather();
+//   }, [incrementFather])
 
 
 //   const incrementFather = () => {
--- src/memos/ShowIncrement.jsx
+++ src/memos/ShowIncrement.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export const ShowIncrement = React.memo(({ increment }) => {
   console.log("me volvi a renderizar");
   return (
-    <button className="btn btn-primary" onClick={() => increment()}>
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        increment(5);
+      }}
+    >
       Incrementar
     </button>
   );
