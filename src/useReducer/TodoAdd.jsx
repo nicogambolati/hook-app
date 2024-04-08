fe2efc6111a5ac238186404142462bcbba3c6b4a
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-08 19:36:16 -0300

    splits TodoApp into several components

---
+++ src/useReducer/TodoAdd.jsx
@@ -0,0 +1,35 @@
+import { useForm } from "../hooks/useForm";
+
+export const TodoAdd = ({ onNewTodo }) => {
+  const { description, onInputChange, onResetForm } = useForm({
+    description: "",
+  });
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (description.length <= 1) return;
+    const newTodo = {
+      id: new Date().getTime(),
+      description: description,
+      done: false,
+    };
+    onNewTodo(newTodo);
+    onResetForm();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="¿Que hay que hacer?"
+        className="form-control"
+        name="description"
+        value={description}
+        onChange={onInputChange}
+      />
+      <button className="btn btn-outline-primary mt-1 btn-block">
+        Agregar
+      </button>
+    </form>
+  );
+};
--- src/useReducer/TodoApp.jsx
+++ src/useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import { todoReducer } from "./todoReducer";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
 
 const initialState = [
   {
@@ -9,7 +11,7 @@ const initialState = [
   },
   {
     id: new Date().getTime() * 3, // por si da la casualidad que sea muy rapido y sea el mismo id
-    description: "Recolectar la piedra del poder",
+    description: "Recolectar la piedra del tiempo",
     done: false,
   },
 ];
@@ -17,40 +19,24 @@ const initialState = [
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
+  const handleNewTodo = (todo) => {
+    console.log({ todo });
+  };
+
   return (
     <>
       <h1>
         TodoApp: 10, <small>pendientes: 2</small>
       </h1>
       <hr />
-
       <div className="row">
         <div className="col-7">
-          <ul className="list-group">
-          {
-            todos.map (todo => (
-              <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                <span className="align-self-center">Item 1</span>
-                <button className="btn btn-danger">Borrar</button>
-              </li>
-            ))
-          }
-          </ul>
+          <TodoList todos={todos} />
         </div>
-
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Que hay que hacer?"
-              className="form-control"
-            />
-            <button className="btn btn-outline-primary mt-1 btn-block">
-              Agregar
-            </button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
